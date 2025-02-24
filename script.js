7d6c8032f0faeefc9de5842d19343836f3712dbe
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-02-24 23:10:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+        document.getElementById("change_button").addEventListener("click", function() {
+            const blockId = document.getElementById("block_id").value;
+            const color = document.getElementById("colour_id").value;
+            
+            document.querySelectorAll(".grid-item").forEach(item => {
+                item.style.backgroundColor = "transparent";
+            });
+            
+            if (blockId >= 1 && blockId <= 9) {
+                document.getElementById(blockId).style.backgroundColor = color;
+            }
+        });
+        
+        document.getElementById("Reset").addEventListener("click", function() {
+            document.querySelectorAll(".grid-item").forEach(item => {
+                item.style.backgroundColor = "transparent";
+            });
+        });
+    </script>
+</body>
+
